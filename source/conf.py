e8987b8fc5a62de1722d75be58690fd94416a509
Nabil ngouhouo <nabilf045@gmail.com>
2025-04-18 08:26:26 +0100

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -25,5 +25,5 @@ language = 'fr'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'ablaster'
 
